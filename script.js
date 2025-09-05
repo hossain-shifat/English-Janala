abcf93e55f74a01d8fd9b39fbf844754c6f00f9c
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-09-05 10:40:26 +0600

    Add learning section

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const loadLessons =()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then(res => res.json())
+    .then(json => displayLesson(json.data))
+}
+const displayLesson =(lessons) =>{
+    const lavelContainer = document.getElementById("lavel-container")
+    lavelContainer.innerHTML = ""
 
+    for(lesson of lessons){
+        const btnDiv = document.createElement("div")
+        btnDiv.innerHTML = `
+        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no}</button>
+        `
+        lavelContainer.append(btnDiv)
+    }
+
+}
+loadLessons()
